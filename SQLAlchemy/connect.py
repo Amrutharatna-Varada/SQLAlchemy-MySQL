589042a6c7a0e9bc36237330d1b9c3586e431e9b
Amrutharatna-Varada <66815331+Amrutharatna-Varada@users.noreply.github.com>
2022-01-16 12:16:50 +0530

    Update connect.py

--- SQLAlchemy/connect.py
+++ SQLAlchemy/connect.py
@@ -3,7 +3,7 @@ import sqlalchemy as db
 engine = db.create_engine("mysql://root:12345678@localhost:3306/organisation")
 c=engine.connect()
 md=db.MetaData()
-org=db.Table(employee,md,autoload=True,autoload_with=engine)
+org=db.Table('employee',md,autoload=True,autoload_with=engine)
 wh=db.select([employee]).where(employee.columns.salary < 15000)
 rw=c.execute(wh).fetchall()
 i_n=db.select(employee]).where(employee.columns.salary.in_([2500,5000,7500]))
